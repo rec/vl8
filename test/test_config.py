cc50daabcad5608d0136081846503460392ce075
Tom Ritchford <tom@swirly.com>
2020-11-26 16:55:38 +0100

    Tests for config

--- test/test_config.py
+++ test/test_config.py
@@ -13,3 +13,36 @@ class TestConfig(unittest.TestCase):
             assert (
                 m.exception.args[0] == 'Do not understand section "%s"' % bad
             )
+
+    def test_merge(self):
+        assert config.merge([]) == {}
+
+        c1 = {'so': {'foo': ['a', 'b']}, 'fu': {'bar': {'bing': 'bong'}}}
+
+        assert config.merge([]) == {}
+        expected = {
+            'source': {'foo': ['a', 'b']},
+            'function': {'bar': {'bing': 'bong'}},
+        }
+        assert config.merge([c1]) == expected
+
+        c2 = {
+            'source': {'foo': ['c', 'd']},
+            'function': {'bar': {'bop': 'bop'}},
+            'job': {},
+        }
+        assert config.merge([c2]) == c2
+
+        expected = {
+            'function': {'bar': {'bop': 'bop'}},
+            'job': {},
+            'source': {'foo': ['c', 'd']},
+        }
+        assert config.merge([c1, c2]) == expected
+
+        expected = {
+            'function': {'bar': {'bing': 'bong', 'bop': 'bop'}},
+            'job': {},
+            'source': {'foo': ['a', 'b', 'c', 'd']},
+        }
+        assert config.merge([c1, c2], overwrite=False) == expected
