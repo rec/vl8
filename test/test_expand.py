670485060ddba3a5d7b178cbd2e86366f0e4349f
Tom Ritchford <tom@swirly.com>
2020-11-30 19:04:57 +0100

    Fix a bug in Expander
    * Found by increasing test coverage to 100%

--- test/test_expand.py
+++ test/test_expand.py
@@ -1,7 +1,7 @@
 from vl8.config.expand import Expander
 import unittest
 
-expander = Expander('config', {'money': 3, 'love': 10})
+expander = Expander('config', {'money': 3, 'love': 10, 'murder': 0})
 
 
 class TestExpander(unittest.TestCase):
@@ -12,20 +12,23 @@ class TestExpander(unittest.TestCase):
     def test_empty(self):
         self.expand()
         assert self.errors == []
-        assert self.config == {'money': 3, 'love': 10}
+        assert self.config == {'money': 3, 'love': 10, 'murder': 0}
 
     def test_simple(self):
-        self.expand(m=5)
+        self.expand(money=5)
         assert self.errors == []
-        assert self.config == {'money': 5, 'love': 10}
+        assert self.config == {'money': 5, 'love': 10, 'murder': 0}
 
         self.expand(mo=5, love=20)
         assert self.errors == []
-        assert self.config == {'money': 5, 'love': 20}
+        assert self.config == {'money': 5, 'love': 20, 'murder': 0}
 
     def test_errors(self):
+        self.expand(m=5)
+        assert self.errors == ["Ambiguous config m=5: ['money', 'murder']"]
+
         self.expand(p=5)
-        assert self.errors == ["Unknown config {'p': 5}"]
+        assert self.errors == ["Unknown config: p=5"]
 
         self.expand(loves=5)
-        assert self.errors == ["Unknown config {'loves': 5}"]
+        assert self.errors == ["Unknown config: loves=5"]
--- vl8/config/expand.py
+++ vl8/config/expand.py
@@ -9,19 +9,18 @@ class Expander:
     def __call__(self, config):
         results = {}
 
+        for k, v in config.items():
+            opts = [d for d in self.defaults if d.startswith(k)]
+            if opts:
+                if len(opts) > 1:
+                    yield f'Ambiguous {self.name} {k}={v}: {opts}'
+                results[opts[0]] = v
+            else:
+                yield f'Unknown {self.name}: {k}={v}'
+
         for k, v in self.defaults.items():
-            opts = [c for c in list(config) if k.startswith(c)]
-            if len(opts) > 1:
-                yield f'Ambiguous {self.name} {opts}'
-            values = [config.pop(c) for c in opts]
-            if values:
-                v = values[-1]
-            if callable(v):
-                v = v()
-            results[k] = v
+            if k not in results:
+                results[k] = v() if callable(v) else v
 
-        if config:
-            yield f'Unknown {self.name} {config}'
-            config.clear()
-
+        config.clear()
         config.update(results)
