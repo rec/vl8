62fb88a49a3a21e28eca8484ec95f5d67cd466b1
Tom Ritchford <tom@swirly.com>
2020-12-03 17:41:10 +0100

    Fix a SEGFAULT bug
    * How aubio can do this needs further explanation

--- test/test_io.py
+++ test/test_io.py
@@ -1,5 +1,7 @@
-from numpy.testing import assert_array_equal
+# from numpy.testing import assert_array_equal
+from vl8 import npf
 from vl8 import util
+import aubio
 import numpy as np
 import tdir
 import unittest
@@ -9,18 +11,59 @@ DURATION = 2 + 2 * util.MAX_FRAME
 
 @tdir
 class TestIO(unittest.TestCase):
-    def NO_test_round_trip(self):
+    def test_round_trip(self):
         half = DURATION // 2
 
-        b1 = util.linspace(0, half - 1, half)
-        b2 = util.linspace(half, DURATION - 1, half, dtype='int32')
+        b1 = npf.linspace(0, half - 1, half)
+        b2 = npf.linspace(half, DURATION - 1, half)
 
         b1 = b1.reshape(1, half)
         b2 = b2.reshape(1, half)
-        b1 = np.r_[b1, b1]
-        b2 = np.r_[b2, b2]
+        b1 = npf.r_[b1, b1]
+        b2 = npf.r_[b2, b2]
         util.write('b1.wav', b1)
         util.write('b2.wav', b2)
 
-        assert_array_equal(b1, util.read('b1.wav'))
-        assert_array_equal(b2, util.read('b2.wav'))
+        c1 = util.read('b1.wav')
+        if True:
+            return
+        assert c1.shape == (2, half)
+
+        total = util.read(('b1.wav', 'b2.wav'))
+        assert total.shape == (2, DURATION)
+
+    def test_round_trip_2(self):
+        import aubio
+
+        length = 100
+
+        buf = np.linspace(0, length - 1, length, dtype=np.float32)
+        buf = buf.reshape(1, length)
+        buf = npf.r_[buf, buf]
+        assert buf.shape == (2, length)
+        if not False:
+            sink = aubio.sink('test.wav', channels=2)
+            sink.do_multi(buf, length)
+            sink.close()
+
+            chunks = list(aubio.source('test.wav'))
+            print(len(chunks), chunks[0].shape)
+
+        c1 = util.read('test.wav')
+        assert c1.shape == (2, length)
+
+    def test_round_trip_3(self):
+        length = 100
+        buf = np.zeros((2, length), dtype=np.float32)
+
+        assert buf.shape == (2, length)
+        with aubio.sink('test.wav', channels=2) as sink:
+            sink.do_multi(buf, length)
+
+        src = aubio.source('test.wav', channels=2)
+        assert src.duration == length
+
+        (chunk,) = list(src)
+
+        assert chunk.shape[1] == length
+        assert src.duration == length
--- vl8/util.py
+++ vl8/util.py
@@ -5,6 +5,8 @@ import sys
 
 MAX_FRAME = 4096
 VERBOSE = True
+CAUSE_CRASH = False
+# Track down a crash in aubio
 
 
 def source(f):
@@ -14,12 +16,21 @@ def source(f):
 def read(filenames, grain_size=0, duration=0, gap=0):
     if isinstance(filenames, (str, Path)):
         filenames = [filenames]
+
+    assert filenames, 'No filenames'
+
     if not duration:
         duration = sum(source(f).duration for f in filenames)
-        duration += gap * len(filenames) - 1
+        if not CAUSE_CRASH:
+            duration += gap * (len(filenames) - 1)
+        else:
+            # Bug that triggered a crash
+            duration += gap * len(filenames) - 1
 
-    if grain_size:
-        buffer = npf.empty(duration + (-duration % grain_size))
+    fill = grain_size and -duration % grain_size
+    if fill:
+        buffer = npf.empty(duration + fill)
+        buffer[:, -fill:].fill(0)
     else:
         buffer = npf.empty(duration)
 
@@ -35,9 +46,7 @@ def read(filenames, grain_size=0, duration=0, gap=0):
             end = min(duration, begin + chunk.shape[1])
             buffer[:, begin:end] = chunk[:, : end - begin]
             begin = end
-        else:
-            if begin < duration:
-                buffer[:, begin:].fill(0)
+
         begin += gap
 
     if begin != duration + gap:
@@ -58,7 +67,8 @@ def write(filename, buffer):
 
     for o in range(0, nsamples, MAX_FRAME):
         length = min(MAX_FRAME, nsamples - o)
-        sink.do_multi(buffer[:, o : o + length], length)
+        buf = buffer[:, o : o + length]
+        sink.do_multi(buf, length)
 
 
 def error(*args, **kwds):
