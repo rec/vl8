ee9e60286a2159563806a2ef0917b52e9a15b649
Tom Ritchford <tom@swirly.com>
2020-12-08 13:02:32 +0100

    Cosmetic changes to grains.py

--- vl8/grains.py
+++ vl8/grains.py
@@ -13,25 +13,22 @@ class Grains:
 
     def __post_init__(self):
         assert 0 <= self.overlap <= 1
-        self.fade = round(self.overlap * self.size / 2)
-        self.fade_in = np.linspace(
-            0, 1, self.fade, endpoint=True, dtype=self.dtype
-        )
+        f = round(self.overlap * self.size / 2)
+        self.fade_in = np.linspace(0, 1, f, endpoint=True, dtype=self.dtype)
         self.fade_out = np.flip(self.fade_in)
 
     def chunk(self, i):
         begin = i * (self.size - self.fade)
         c = self.data[:, begin : begin + self.size]
         missing = self.size - c.shape[1]
-        if missing > 0:
-            c = np.c_[c, np.zeros((2, missing), dtype=self.dtype)]
-
-        return c
+        if missing < 0:
+            return c
+        return np.c_[c, np.zeros((2, missing), dtype=self.dtype)]
 
     def add_to(self, i, target):
         c = self.chunk(i)
-        f = self.fade
+        f = len(self.fade_in)
 
         target[:, :f] += c[:, :f] * self.fade_in
         target[:, f : self.size - f] += c[:, f:-f]
-        target[:, self.size - f : self.size] += c[:, -f:]
+        target[:, self.size - f : self.size] += c[:, -f:] * self.fade_out
