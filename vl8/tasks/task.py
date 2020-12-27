1ebd2716c9c23ef805f96a307728ce0af8373f9f
Tom Ritchford <tom@swirly.com>
2020-12-27 18:50:19 +0100

    Flesh out task.py

--- vl8/tasks/task.py
+++ vl8/tasks/task.py
@@ -1,20 +1,30 @@
+from abc import abstractmethod, ABC
+
+
+# Inherit from this!
+# All the behavior goes here and none of the mutability,
+# You can run many Tasks from a singleton TaskDescription.
+# Maybe this is what is called "Function" in the exterior
+# documentation?!
+class TaskDescription(ABC):
+    def __call__(self, config: dict) -> 'Task':
+        return Task(self, **self._config(config))
+
+    @abstractmethod
+    def _config(self, config: dict) -> dict:
+        return config
+
+    @abstractmethod
+    def _run(self, task: 'Task'):
+        return task
+
+
 # All the mutability goes here and none of the behavior.
 class Task:
-    def __init__(self, desc, **kwargs):
+    def __init__(self, desc: TaskDescription, **kwargs):
         self._desc = desc
         for k, v in kwargs.items():
             setattr(self, k, v)
 
     def __call__(self):
-        return self._desc.run(self)
-
-
-# All the behavior goes here and none of the mutability,
-# You can run many Tasks from a singleton TaskDescription
-class TaskDescription:
-    def __call__(self, config: dict):
-        foo, bar, shape = config  # Fake
-        return Task(self, foo=foo, bar=bar, shape=shape)
-
-    def run(self, task: Task):
-        return
+        return self._desc._run(self)
