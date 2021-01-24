7b876cddc8d3820c8eae0af12368ecea29d6f2cc
Tom Ritchford <tom@swirly.com>
2021-01-24 12:05:51 +0100

    New reflect.py sketch

---
+++ vl8/tasks/reflect.py
@@ -0,0 +1,25 @@
+# from ..grain import Grain
+from . import task
+import numpy as np
+
+MIN_STRIPE_SIZE = 10
+
+""""
+reflect is a granular task that mixes a sound with its mirror image
+"""
+
+
+class Reflect(task.TaskDescription):
+    def _config(self, config: dict) -> dict:
+        return config
+
+    def _run(self, task: task.Task):
+        return task
+
+
+reflect = np.flip
+
+"""
+TODO: This works perfectly without an adaptor, so how can we make sure it all
+works without any other fiddling?
+"""
