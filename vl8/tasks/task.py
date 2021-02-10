fff7a1faf5b43662bf06f56ba69f36e11e1ac7b0
Tom Ritchford <tom@swirly.com>
2021-02-10 13:21:28 +0100

    Simpler tasks

--- vl8/tasks/task.py
+++ vl8/tasks/task.py
@@ -1,61 +1,5 @@
-
-# from collections import namedtuple
-from abc import abstractmethod, ABC
-from dataclasses import dataclass, field
+from typing import Class
 import dataclasses
-
-
-class Context:
-    def __init__(self, *args, **kwargs):
-        """
-        args: is typically a list of files or of numpy samples
-        kwargs: is a dictionary -  a pool of variables that can be passed
-          forward.  Unlike kwargs in a function, not all variables must be used
-
-        """
-        self.args = args
-        self.kwargs = kwargs
-
-    def __call__(self, f):
-        pass
-        # f(*self.args, **self.kwargs)
-
-
-# Inherit from this!
-# All the behavior goes here and none of the mutability,
-# You can run many Tasks from a singleton TaskDescription.
-# Maybe this is what is called "Function" in the exterior
-# documentation?!
-class TaskDescription(ABC):
-    def __call__(self, config: dict) -> 'Task':
-        return Task(self, **self._config(config))
-
-    @abstractmethod
-    def _config(self, config: dict) -> dict:
-        return config
-
-    @abstractmethod
-    def _run(self, task: 'Task', context):
-        return context
-
-
-# All the mutability goes here and none of the behavior.
-class Task:
-    def __init__(self, desc: TaskDescription, **kwargs):
-        self._desc = desc
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-
-    def __call__(self, context):
-        return self._desc._run(self, context)
-
-
-@dataclass
-class Test:
-    one: int
-    two: dict = field(default_factory=dict)
-    three: str = 'Three'
-
 
 """
 Each task description is represented by a dataclass.
@@ -75,7 +19,20 @@ But a dataclass with a single call method might be really neat.
 """
 
 
-def construct_from(context, data_cls):
-    field_names = {f.name for f in dataclasses.fields(data_cls)}
-    kwargs = {k: v for k, v in context.items() if k in field_names}
-    return data_cls(**kwargs)
+class Task:
+    def __init__(self, data_cls: Class, **kwargs):
+        self.field_names = {f.name for f in dataclasses.fields(data_cls)}
+        missing = set(kwargs) - self.field_names
+        if missing:
+            s = '' if len(missing) == 1 else 's'
+            raise ValueError(
+                f'Unknown field{s}: {missing} (Valid are {self.field_names})'
+            )
+
+        self.data_cls = data_cls
+        self.kwargs = kwargs
+
+    def construct(self, **context):
+        context = dict(self.kwargs, **context)
+        context = {k: v for k, v in context.items() if k in self.field_names}
+        return self.data_cls(**context)
