a9a55967969a51b3ea989bcb225b60823eec2466
Tom Ritchford <tom@swirly.com>
2021-03-08 14:05:27 +0100

    Refactor function.py slightly

--- vl8/function/function.py
+++ vl8/function/function.py
@@ -4,40 +4,48 @@ import inspect
 
 class Function:
     def __init__(self, name):
-        self.name = name
-        f = self.function = importer(name)
-        self.params = _params(f)
-        self.is_class = isinstance(f, type)
+        def params(s):
+            return dict(inspect.signature(s).parameters)
 
-        if self.is_class:
-            f_call = vars(f).get('__call__')
+        self.name = name
+        self.function = importer(name)
+        self.params = params(self.function)
+        self.is_class = isinstance(self.function, type)
+
+        if not self.is_class:
+            # It's a function
+            if not self.params:
+                raise ValueError(
+                    f'{self.function}() needs at least one argument'
+                )
+            pname = next(iter(self.params))
+            param = self.params.pop(pname)
+
+        else:
+            f_call = vars(self.function).get('__call__')
             if not f_call:
                 raise ValueError(f'Class {name} is not callable')
 
-            params = list(_params(f_call).values())
+            call_params = list(params(f_call).values())
 
-            if not params or params[0].name != 'self':
-                raise ValueError(f'{f}.__call__ must be a member function')
-
-            if len(params) != 2:
-                raise ValueError(f'{f}.__call__() takes one argument')
-
-            param = params[1]
-
-        elif not self.params:
-            raise ValueError(f'{f}() needs at least one argument')
-
-        else:
-            # It's a function
-            param = next(iter(self.params.values()))
-            self.params.pop(param.name)
+            if not call_params or call_params[0].name != 'self':
+                raise ValueError(
+                    f'{self.function}.__call__() must be a member function'
+                )
+            if len(call_params) != 2:
+                raise ValueError(
+                    f'{self.function}.__call__() takes one argument'
+                )
+            param = call_params[1]
 
         if param.kind in (param.POSITIONAL_ONLY, param.POSITIONAL_OR_KEYWORD):
             self.is_simple = True
         elif param.kind is param.VAR_POSITIONAL:
             self.is_simple = False
         else:
-            raise ValueError(f'Bad signature for {f}({self.params})')
+            raise ValueError(
+                f'Bad signature for {self.function}({self.params})'
+            )
 
     def required(self):
         return [p.name for p in self.params.values() if p.default is p.empty]
@@ -49,7 +57,3 @@ class Function:
             return self.function(**kwargs)(*sources)
         else:
             return self.function(*sources, **kwargs)
-
-
-def _params(s):
-    return dict(inspect.signature(s).parameters)
