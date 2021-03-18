65c19d63d2bbf52eb978d4e5fac432d939674f12
Tom Ritchford <tom@swirly.com>
2021-03-18 12:22:49 +0100

    Add a flag to overwrite results

--- test/assert_files_equal.py
+++ test/assert_files_equal.py
@@ -1,14 +1,25 @@
 from pathlib import Path
 from vl8.dsp import io
 
+WRITE_RESULTS = False
 
-def assert_files_equal(filename, result, sample_rate):
+
+def assert_files_equal(
+    filename, result, sample_rate, write_results=WRITE_RESULTS
+):
     io.write(filename, result, sample_rate=sample_rate)
+    if write_results:
+        io.write(results_file(filename), result, sample_rate=sample_rate)
+
     assert_equal(filename)
 
 
+def results_file(filename):
+    return Path(__file__).parent / 'dsp/results' / filename
+
+
 def assert_equal(filename):
-    rfile = Path(__file__).parent / 'dsp/results' / filename
-    d1, d2 = Path(filename).read_bytes(), rfile.read_bytes()
+    d1 = Path(filename).read_bytes()
+    d2 = results_file(filename).read_bytes()
     assert len(d1) == len(d2), f'{len(d1)} == {len(d2)}'
     assert d1 == d2
