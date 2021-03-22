ff792f136f432733f881773c98ac69d73426f48f
Tom Ritchford <tom@swirly.com>
2021-03-22 12:54:35 +0100

    [split] Modify vl8/dsp/fade.py

--- vl8/dsp/fade.py
+++ vl8/dsp/fade.py
@@ -4,13 +4,20 @@ import xmod
 
 @xmod
 def fade(src: np.ndarray, fade_in: np.ndarray, fade_out: np.ndarray) -> None:
+    if not src.size:
+        return
+
     # TODO: unsafe leaves the possibility of overs in a fixed-point format.
     def mul(x, y):
         np.multiply(x, y, out=x, casting='unsafe')
 
-    half = src.shape[1] // 2
-    fi = min(len(fade_in), half)
-    fo = min(len(fade_out), half)
+    fi = len(fade_in)
+    fo = len(fade_out)
+
+    ratio = (fi + fo) / src.shape[1]
+    if ratio and ratio > 1:
+        fi = round(fi / ratio)
+        fo = src.shape[1] - fi
 
     if fi:
         mul(src[:, :fi], fade_in[:fi])
