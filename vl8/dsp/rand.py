4180cb9205a8077c83f2e080a2423bb527aab1d4
Tom Ritchford <tom@swirly.com>
2021-03-28 17:37:34 +0200

    Remove old random number code

--- vl8/dsp/rand.py
+++ vl8/dsp/rand.py
@@ -1,29 +1,19 @@
 from .scale import Scale
 from dataclasses import dataclass, field
 from typing import Callable, Optional
-import numpy as np
-import random
-
-USE_NUMPY = True
-
-if USE_NUMPY:
-    Random = np.random.Generator
-    make_random = np.random.default_rng
-else:
-    Random = random.Random
-    make_random = Random
+import numpy.random as npr
 
 
 @dataclass
 class Rand:
-    distribution: Callable = Random.uniform
+    distribution: Callable = npr.Generator.uniform
     args: list = field(default_factory=list)
     kwargs: dict = field(default_factory=dict)
     seed: Optional[int] = None
     scale: Scale = field(default_factory=Scale)
 
     def __post_init__(self):
-        self._inst = make_random(self.seed)
+        self._inst = npr.default_rng(self.seed)
 
     def __call__(self, *args, **kwargs):
         if not args:
