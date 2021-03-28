10ed1d4ead86cf974d1ae376e5b7f4b1ce604755
Tom Ritchford <tom@swirly.com>
2021-03-28 17:35:46 +0200

    Allow Rand to use the numpy random number generations

--- vl8/dsp/rand.py
+++ vl8/dsp/rand.py
@@ -1,21 +1,29 @@
 from .scale import Scale
 from dataclasses import dataclass, field
 from typing import Callable, Optional
+import numpy as np
 import random
+
+USE_NUMPY = True
+
+if USE_NUMPY:
+    Random = np.random.Generator
+    make_random = np.random.default_rng
+else:
+    Random = random.Random
+    make_random = Random
 
 
 @dataclass
 class Rand:
-    distribution: Callable = random.Random.uniform
+    distribution: Callable = Random.uniform
     args: list = field(default_factory=list)
     kwargs: dict = field(default_factory=dict)
     seed: Optional[int] = None
     scale: Scale = field(default_factory=Scale)
 
     def __post_init__(self):
-        self._inst = random.Random()
-        if self.seed is not None:
-            self._inst.seed(self.seed)
+        self._inst = make_random(self.seed)
 
     def __call__(self, *args, **kwargs):
         if not args:
