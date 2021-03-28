186906b08098610fa2ff12b4f07e0fb12e0663ef
Tom Ritchford <tom@swirly.com>
2021-03-28 17:55:46 +0200

    Improve rand.py

--- vl8/dsp/rand.py
+++ vl8/dsp/rand.py
@@ -1,19 +1,22 @@
 from .scale import Scale
 from dataclasses import dataclass, field
-from typing import Callable, Optional
-import numpy.random as npr
+from numpy.random import Generator, default_rng
+from typing import Callable, Optional, Union
+
+Distribution = Union[Callable, str, None]
+DEFAULT_DISTRIBUTION = 'uniform'
 
 
 @dataclass
 class Rand:
-    distribution: Callable = npr.Generator.uniform
+    distribution: Distribution = None
     args: list = field(default_factory=list)
     kwargs: dict = field(default_factory=dict)
     seed: Optional[int] = None
     scale: Scale = field(default_factory=Scale)
 
     def __post_init__(self):
-        self._inst = npr.default_rng(self.seed)
+        self._inst = default_rng(self.seed)
 
     def __call__(self, *args, **kwargs):
         if not args:
@@ -26,3 +29,19 @@ class Rand:
             kwargs = dict(self.kwargs, **kwargs)
 
         return self.scale(self.distribution(self._inst, *args, **kwargs))
+
+    def _set_distribution(self, d: Distribution):
+        d = DEFAULT_DISTRIBUTION if d is None else d
+        if callable(d):
+            self._distribution = d
+        else:
+            try:
+                self._distribution = getattr(Generator, d)
+            except AttributeError:
+                raise ValueError(f'No distribution {d}') from None
+
+    def _get_distribution(self) -> Callable:
+        return self._distribution
+
+
+Rand.distribution = property(Rand._get_distribution, Rand._set_distribution)
