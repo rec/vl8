8689d57cb9c2a37d4903286e94b3eb029b7e8a6c
Tom Ritchford <tom@swirly.com>
2021-04-01 13:17:30 +0200

    Change default directory for output

--- vl8/dsp/io.py
+++ vl8/dsp/io.py
@@ -5,6 +5,7 @@ import wavemap
 
 read = pydub_io.read
 write = pydub_io.write
+DEFAULT_DIR = '_vl8'
 
 
 def write_data(data_list, output_file=None, output_format='.wav', dtype=None):
@@ -25,4 +26,4 @@ def write_data(data_list, output_file=None, output_format='.wav', dtype=None):
 
 
 def _timestamp():
-    return datetime.datetime.now().strftime('VL8/%Y-%m-%d-%H-%M-%S')
+    return datetime.datetime.now().strftime(f'{DEFAULT_DIR}/%Y-%m-%d-%H-%M-%S')
